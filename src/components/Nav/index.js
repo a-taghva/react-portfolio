73cccf1473d05d5e6fdae07ebf0291b8fe8606e8
a-taghva <taghvali99@gmail.com>
2021-05-01 21:56:17 -0400

    navActive

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Nav() {
+  const nav = [
+    {
+      'title': 'About Me',
+      'link': '#about-me',
+    },
+    {
+      'title': 'Wrok',
+      'link': '#work',
+    },
+    {
+      'title': 'Contact Me',
+      'link': '#contact-me',
+    },
+    {
+      'title': 'Resume',
+      'link': '#resume',
+    },
+  ];
+
+  const [navSelected, setNavSelected] = useState(nav[0])
+
   return(
     <header>
       <h1>Portfolio</h1>
         <nav>
             <ul class="anchor-style">
-                <li><a href="#about-me">About Me</a></li>
-                <li><a href="#work">Work</a></li>
-                <li><a href="#contact-me">Contact Me</a></li>
-                <li><a href="#">Resume</a></li>
+              {nav.map(navEl => (
+                <li 
+                  key={navEl.title} 
+                  className={navEl.title === navSelected.title ? 'navActive' : ''}
+                  onClick={() => setNavSelected(navEl)}
+                >
+                  <a href={navEl.link}>{navEl.title}</a>
+                </li>
+              ))}
             </ul>
         </nav>
     </header>
