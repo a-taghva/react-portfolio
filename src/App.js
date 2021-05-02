0c484b01e796a83f4475fb96402707205f5e806c
a-taghva <taghvali99@gmail.com>
2021-05-01 23:35:47 -0400

    about me section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'; 
 import Nav from './components/Nav'
+import About from './components/About'
 import './App.css';
 
 function App() {
   return (
-    <Nav />
+    <>
+      <Nav />
+      <About />
+    </>
   );
 }
 
---
+++ src/components/About/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function About() {
+  return(
+    <section id="about-me">
+      <i class="fa fa-user-circle"></i>
+      <h2 class="section-title">About Me</h2>
+      <div>
+          <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Sint magnam mollitia, fugiat provident fuga numquam quae nam 
+              earum maiores alias voluptates ratione facilis esse exercitationem 
+              blanditiis maxime non eaque molestias. Impedit corrupti voluptate 
+              animi eaque amet dolores iste, eveniet fuga!
+          </p>
+          <br />
+          <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+              Quos minima a molestias ipsum temporibus, perferendis accusantium 
+              quod magnam fugiat dolorem, modi facilis iusto vitae doloribus inventore? 
+              Est debitis ipsa eveniet!
+          </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
