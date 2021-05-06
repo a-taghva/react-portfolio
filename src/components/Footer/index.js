5623acc2a3830d5923b1d4cdadb3de3bc9dd4289
a-taghva <taghvali99@gmail.com>
2021-05-06 13:24:59 -0400

    change section to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Footer() {
   return(
-    <section id="footer">
+    <footer id="footer">
       <div id="social-media">
         <a href="https://www.github.com/a-taghva"><i className="fa fa-github"></i></a>
         <a href="https://www.linkedin.com/in/ali-taghva/"><i className="fa fa-linkedin"></i></a>
         <a href="https://twitter.com/taghva_ali"><i className="fa fa-twitter"></i></a>
       </div>
-    </section>
+    </footer>
   );
 }
 
