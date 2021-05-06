e11dd0cee0ddadec093f4334e1c983be9d6feff1
a-taghva <taghvali99@gmail.com>
2021-05-06 11:16:50 -0400

    add footer component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Resume from './components/Resume'
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -53,6 +54,7 @@ function App() {
         setNavSelected={setNavSelected}
       />
       {renderComponent(navSelected.component)}
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return(
+    <section id="footer">
+      <p>&copy; Ali Taghva 2021</p>
+    </section>
+  );
+}
+
+export default Footer;
