049f5f2fa4d2e539d3c96e6b30c110bfdaa5fc53
a-taghva <taghvali99@gmail.com>
2021-05-06 12:25:26 -0400

    add twitter to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,6 +6,7 @@ function Footer() {
       <div id="social-media">
         <a href="https://www.github.com/a-taghva"><i className="fa fa-github"></i></a>
         <a href="https://www.linkedin.com/in/ali-taghva/"><i className="fa fa-linkedin"></i></a>
+        <a href="https://twitter.com/taghva_ali"><i className="fa fa-twitter"></i></a>
       </div>
     </section>
   );
